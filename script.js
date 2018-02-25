3358a638226f51d4e6e1236dc85cc962bc7ed63d
shreshth28 <shreshth28srivastava@gmail.com>
2018-02-26 00:36:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 	$(this).remove();
 	});
-	event.stopPropogation();
+	event.stopPropagation();
 });
 $("input[type='text']").keypress(function(e){
 	if(e.which===13)
